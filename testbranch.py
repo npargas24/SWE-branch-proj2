d51ab363936f9b352d9468b23371619db4987305
npargas24 <114077181+npargas24@users.noreply.github.com>
2025-02-14 13:42:42 -0500

    added soup sandwich test

--- testbranch.py
+++ testbranch.py
@@ -1,1 +1,19 @@
+import unittest
+
+def testSandwich(sandwich):
+    firstItem = [sandwich[0]]
+    lastItem = [sandwich[-1]]
+    if firstItem == lastItem:
+        print("it's a sandwich")
+        return True
+    else:
+        return False
+
+def testSoup(soup):
+    for item in soup:
+        if item == 'broth' or 'liquid':
+            print("it's a soup")
+            return True
+
+
 
